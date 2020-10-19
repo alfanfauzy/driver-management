8fadb70940b8d38c9c3bb3006313ba8269c5ab3e
Alfan Fauzy <zhafhy@gmail.com>
2020-10-19 19:30:28 +0700

    Updates

---
+++ precache-manifest.e5fb71f3f8e2787cb1726344a4857db4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "139b30a08f6a261fcc443fcb0f227e71",
+    "url": "/driver-management/index.html"
+  },
+  {
+    "revision": "5ada59897db90cb6a64b",
+    "url": "/driver-management/static/css/main.fda12228.chunk.css"
+  },
+  {
+    "revision": "9110666cf5059bf3620c",
+    "url": "/driver-management/static/js/2.c66e791c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/driver-management/static/js/2.c66e791c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ada59897db90cb6a64b",
+    "url": "/driver-management/static/js/main.f662e39f.chunk.js"
+  },
+  {
+    "revision": "e9ea57dc851a97150f3a",
+    "url": "/driver-management/static/js/runtime-main.2dad2c7c.js"
+  },
+  {
+    "revision": "e87445d06e45f3afd553c5703daa5fd1",
+    "url": "/driver-management/static/media/shipper.e87445d0.svg"
+  }
+]);
